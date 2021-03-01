a4ebb3374ca2cb3ce2f480a99739322ddeb9a970
juan_carlos_meza <juancmezat@gmail.com>
2021-03-01 00:49:33 -0800

    setting up login

--- src/App.js
+++ src/App.js
@@ -1,63 +1,74 @@
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
 
-function App() {
+class App extends Component {
+
+  state = {
+    username: null,
+    email: null,
+    password: null,
+    login: false,
+    store: null
+
+  }
+
   
-  const fetchTest = () => {
-    fetch('http://localhost:3000/api/v1/users', {
+  login(){
+    fetch('http://localhost:3000/api/v1/login', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
         Accept: 'application/json'
       },
-      body: JSON.stringify({
-        user: {
-          username: "test2",
-          password: "123"
-          
+      body: JSON.stringify(this.state)
+      })
+      .then(res => res.json())
+      .then((res) => {
+        console.log(res);
+        localStorage.setItem('login', JSON.stringify({
+          login: true,
+          token: res.token
+        }))
+      })
 
-        }
-      })
-      })
-      .then(r => r.json())
-      .then(console.log)
   }
-  
-  return (
-    <div className="App">
-      <div>Test</div>
-      <div>Test2</div>
-      <form onSubmit={() => this.fetchTest()}>
-        <label>
-          <input
-            className="form-control form-control-lg"
-            type="text"
-            placeholder="username"
-            id="username"
-            name="username"
-          />{" "}
-          <br />
-        </label>
+  render(){
+    return (
+      <div className="App">
+        <form onSubmit={this.login()}>
+          <label>
+            <input
+              onChange={(e) => this.setState({username: e.target.value})}
+              className="form-control form-control-lg"
+              type="text"
+              placeholder="username"
+              id="username"
+              name="username"
+            />{" "}
+            <br />
+          </label>
 
-        <label>
-          <input
-            className="form-control form-control-lg"
-            type="password"
-            placeholder="password"
-            id="password"
-            name="password"
-          />{" "}
-          <br />
-        </label>
-        <button
-          className="btn btn-outline-light btn-block"
-          type="submit"
-        >
-          Sign up
-        </button>
-      </form>
-    </div>
-  );
+          <label>
+            <input
+              onChange={(e) => this.setState({password: e.target.value})}
+              className="form-control form-control-lg"
+              type="password"
+              placeholder="password"
+              id="password"
+              name="password"
+            />{" "}
+            <br />
+          </label>
+          <button
+            className="btn btn-outline-light btn-block"
+            type="submit"
+          >
+            Sign up
+          </button>
+        </form>
+      </div>
+    );
+  };
 }
 
 export default App;
